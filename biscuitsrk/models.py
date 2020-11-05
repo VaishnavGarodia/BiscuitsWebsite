a10f0c67c9b8400856f143f4c1360c45318f11b2
Vaishnav Garodia <vaishnavgarodia@Vaishnavs-MacBook-Air.local>
2020-11-05 11:26:54 +0530

    made default level 1

--- biscuitsrk/models.py
+++ biscuitsrk/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 class QuestionsModel(models.Model):
-    level = models.IntegerField(default=0)
+    level = models.IntegerField(default=1)
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     def __str__(self):
